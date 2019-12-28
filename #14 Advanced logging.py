d1dd88980521fe1384af0fc7c0875062d1ebbaef
Alex Skorokhod <oleksiy.skorokhod@gmail.com>
2019-12-28 11:32:02 +1100

    first commit

---
+++ #14 Advanced logging.py
@@ -0,0 +1,383 @@
+import random
+import numpy as np
+import pandas as pd
+import os
+from absl import app
+from pysc2.agents import base_agent
+from pysc2.lib import actions, features, units
+from pysc2.env import sc2_env, run_loop
+
+import logging
+
+logging.basicConfig(format='%(asctime)-15s %(message)s')
+fh = logging.FileHandler('logs/#10.log')
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+fh_actions = open('logs/#10_actions_v12.log', "a+")
+
+
+DATA_FILE = 'DQNs/SC2_DQN_G10.gz'
+
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9):
+        self.actions = actions
+        self.learning_rate = learning_rate
+        self.reward_decay = reward_decay
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    def choose_action(self, observation, e_greedy=0.9):
+        self.check_state_exist(observation)
+        if np.random.uniform() < e_greedy:
+            state_action = self.q_table.loc[observation, :]
+            action = np.random.choice(
+                state_action[state_action == np.max(state_action)].index)
+        else:
+            action = np.random.choice(self.actions)
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.reward_decay * self.q_table.loc[s_, :].max()
+        else:
+            q_target = r
+        self.q_table.loc[s, a] += self.learning_rate * (q_target - q_predict)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(pd.Series([0] * len(self.actions),
+                                                         index=self.q_table.columns,
+                                                         name=state))
+
+
+class Agent(base_agent.BaseAgent):
+    actions = ("do_nothing",
+               "harvest_minerals",
+               "build_supply_depot",
+               "build_barracks",
+               "train_marine",
+               "attack")
+
+    agent_name = "dummy"
+    should_log_actions = False
+
+    def init_logging(self, agent_name, should_log_actions):
+        self.agent_name = agent_name
+        self.should_log_actions = should_log_actions
+
+
+    def get_my_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.alliance == features.PlayerRelative.SELF]
+
+    def get_enemy_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_my_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.build_progress == 100
+                and unit.alliance == features.PlayerRelative.SELF]
+
+    def get_enemy_completed_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type
+                and unit.build_progress == 100
+                and unit.alliance == features.PlayerRelative.ENEMY]
+
+    def get_distances(self, obs, units, xy):
+        units_xy = [(unit.x, unit.y) for unit in units]
+        return np.linalg.norm(np.array(units_xy) - np.array(xy), axis=1)
+
+    def step(self, obs):
+        super(Agent, self).step(obs)
+        if obs.first():
+            command_center = self.get_my_units_by_type(
+                obs, units.Terran.CommandCenter)[0]
+            self.base_top_left = (command_center.x < 32)
+        if self.should_log_actions: fh_actions.write("\r\nstep,agent=%s" % self.agent_name)
+
+    def do_nothing(self, obs):
+        if self.should_log_actions : fh_actions.write("[do_nothing],noop,OK")
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def harvest_minerals(self, obs):
+        scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
+        idle_scvs = [scv for scv in scvs if scv.order_length == 0]
+        if self.should_log_actions : fh_actions.write("[harvest_minerals],idle_scvs=%i" % len(idle_scvs))
+        if len(idle_scvs) > 0:
+            mineral_patches = [unit for unit in obs.observation.raw_units
+                               if unit.unit_type in [
+                                   units.Neutral.BattleStationMineralField,
+                                   units.Neutral.BattleStationMineralField750,
+                                   units.Neutral.LabMineralField,
+                                   units.Neutral.LabMineralField750,
+                                   units.Neutral.MineralField,
+                                   units.Neutral.MineralField750,
+                                   units.Neutral.PurifierMineralField,
+                                   units.Neutral.PurifierMineralField750,
+                                   units.Neutral.PurifierRichMineralField,
+                                   units.Neutral.PurifierRichMineralField750,
+                                   units.Neutral.RichMineralField,
+                                   units.Neutral.RichMineralField750
+                               ]]
+            scv = random.choice(idle_scvs)
+            distances = self.get_distances(obs, mineral_patches, (scv.x, scv.y))
+            mineral_patch = mineral_patches[np.argmin(distances)]
+            if self.should_log_actions : fh_actions.write(",OK")
+            return actions.RAW_FUNCTIONS.Harvest_Gather_unit(
+                "now", scv.tag, mineral_patch.tag)
+        if self.should_log_actions : fh_actions.write(",FAIL")
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def build_supply_depot(self, obs):
+        supply_depots = self.get_my_units_by_type(obs, units.Terran.SupplyDepot)
+        scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
+
+        if self.should_log_actions : fh_actions.write(
+            "[build_supply_depot],supply_depots=%i minerals=%i scvs=%i" %
+            (
+                len(supply_depots),
+                obs.observation.player.minerals,
+                len(scvs)
+            )
+        )
+
+        if (len(supply_depots) == 0 and obs.observation.player.minerals >= 100 and
+                len(scvs) > 0):
+            supply_depot_xy = (22, 26) if self.base_top_left else (35, 42)
+            distances = self.get_distances(obs, scvs, supply_depot_xy)
+            scv = scvs[np.argmin(distances)]
+
+            if self.should_log_actions : fh_actions.write(",OK")
+            return actions.RAW_FUNCTIONS.Build_SupplyDepot_pt(
+                "now", scv.tag, supply_depot_xy)
+
+        if self.should_log_actions : fh_actions.write(",FAIL")
+        return actions.RAW_FUNCTIONS.no_op()
+
+
+    def build_barracks(self, obs):
+        completed_supply_depots = self.get_my_completed_units_by_type(
+            obs, units.Terran.SupplyDepot)
+        barrackses = self.get_my_units_by_type(obs, units.Terran.Barracks)
+        scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
+
+        if self.should_log_actions : fh_actions.write(
+            "[build_barracks],completed_supply_depots=%i barracks=%i minerals=%i scvs=%i" %
+            (
+                len(completed_supply_depots),
+                len(barrackses),
+                obs.observation.player.minerals,
+                len(scvs)
+            )
+        )
+
+        if (len(completed_supply_depots) > 0 and len(barrackses) == 0 and
+                obs.observation.player.minerals >= 150 and len(scvs) > 0):
+            barracks_xy = (22, 21) if self.base_top_left else (35, 45)
+            distances = self.get_distances(obs, scvs, barracks_xy)
+            scv = scvs[np.argmin(distances)]
+
+            if self.should_log_actions : fh_actions.write(",OK")
+            return actions.RAW_FUNCTIONS.Build_Barracks_pt(
+                "now", scv.tag, barracks_xy)
+
+        if self.should_log_actions : fh_actions.write(",FAIL")
+        return actions.RAW_FUNCTIONS.no_op()
+
+
+    def train_marine(self, obs):
+        completed_barrackses = self.get_my_completed_units_by_type(
+            obs, units.Terran.Barracks)
+        free_supply = (obs.observation.player.food_cap -
+                       obs.observation.player.food_used)
+
+        if self.should_log_actions : fh_actions.write(
+            "[train_marine],completed_barrackses=%i free_supply=%i minerals=%i" %
+            (
+                len(completed_barrackses),
+                free_supply,
+                obs.observation.player.minerals
+            )
+        )
+
+        if (len(completed_barrackses) > 0 and obs.observation.player.minerals >= 100
+                and free_supply > 0):
+            barracks = self.get_my_units_by_type(obs, units.Terran.Barracks)[0]
+
+            if self.should_log_actions : fh_actions.write(" barracks.order_length=%i" % barracks.order_length)
+            if barracks.order_length < 5:
+                if self.should_log_actions : fh_actions.write(",OK")
+                return actions.RAW_FUNCTIONS.Train_Marine_quick("now", barracks.tag)
+
+        if self.should_log_actions : fh_actions.write(",FAIL")
+        return actions.RAW_FUNCTIONS.no_op()
+
+    def attack(self, obs):
+        marines = self.get_my_units_by_type(obs, units.Terran.Marine)
+        if self.should_log_actions : fh_actions.write("[attack],marines=%i" % len(marines))
+        if len(marines) > 0:
+            attack_xy = (38, 44) if self.base_top_left else (19, 23)
+            distances = self.get_distances(obs, marines, attack_xy)
+            marine = marines[np.argmax(distances)]
+            x_offset = random.randint(-4, 4)
+            y_offset = random.randint(-4, 4)
+
+            if self.should_log_actions : fh_actions.write(",OK")
+            return actions.RAW_FUNCTIONS.Attack_pt(
+                "now", marine.tag, (attack_xy[0] + x_offset, attack_xy[1] + y_offset))
+
+        if self.should_log_actions : fh_actions.write(",FAIL")
+        return actions.RAW_FUNCTIONS.no_op()
+
+
+class RandomAgent(Agent):
+    agent_name = "RandomAgent"
+    should_log_actions = False
+
+    def __init__(self):
+        super(RandomAgent, self).__init__()
+        super(RandomAgent, self).init_logging(self.agent_name, self.should_log_actions)
+
+    def step(self, obs):
+        super(RandomAgent, self).step(obs)
+        action = random.choice(self.actions)
+        return getattr(self, action)(obs)
+
+
+class SmartAgent(Agent):
+    agent_name = "SmartAgent"
+    should_log_actions = True
+
+    def __init__(self):
+        super(SmartAgent, self).__init__()
+        super(SmartAgent, self).init_logging(self.agent_name, self.should_log_actions)
+        self.qtable = QLearningTable(self.actions)
+        self.new_game()
+
+        if self.should_log_actions : fh_actions.write("ID,agent,action,function,activity,status\r\n")
+
+        if os.path.isfile(DATA_FILE):
+            logger.info('Load previous learnings')
+            self.qtable.q_table = pd.read_pickle(DATA_FILE, compression='gzip')
+        else:
+            logger.info('NO previous learnings located')
+
+    def reset(self):
+        super(SmartAgent, self).reset()
+        self.new_game()
+
+    def new_game(self):
+        self.base_top_left = None
+        self.previous_state = None
+        self.previous_action = None
+
+    def get_state(self, obs):
+        scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
+        idle_scvs = [scv for scv in scvs if scv.order_length == 0]
+        command_centers = self.get_my_units_by_type(obs, units.Terran.CommandCenter)
+        supply_depots = self.get_my_units_by_type(obs, units.Terran.SupplyDepot)
+        completed_supply_depots = self.get_my_completed_units_by_type(
+            obs, units.Terran.SupplyDepot)
+        barrackses = self.get_my_units_by_type(obs, units.Terran.Barracks)
+        completed_barrackses = self.get_my_completed_units_by_type(
+            obs, units.Terran.Barracks)
+        marines = self.get_my_units_by_type(obs, units.Terran.Marine)
+
+        queued_marines = (completed_barrackses[0].order_length
+                          if len(completed_barrackses) > 0 else 0)
+
+        free_supply = (obs.observation.player.food_cap -
+                       obs.observation.player.food_used)
+        can_afford_supply_depot = obs.observation.player.minerals >= 100
+        can_afford_barracks = obs.observation.player.minerals >= 150
+        can_afford_marine = obs.observation.player.minerals >= 100
+
+        enemy_scvs = self.get_enemy_units_by_type(obs, units.Terran.SCV)
+        enemy_idle_scvs = [scv for scv in enemy_scvs if scv.order_length == 0]
+        enemy_command_centers = self.get_enemy_units_by_type(
+            obs, units.Terran.CommandCenter)
+        enemy_supply_depots = self.get_enemy_units_by_type(
+            obs, units.Terran.SupplyDepot)
+        enemy_completed_supply_depots = self.get_enemy_completed_units_by_type(
+            obs, units.Terran.SupplyDepot)
+        enemy_barrackses = self.get_enemy_units_by_type(obs, units.Terran.Barracks)
+        enemy_completed_barrackses = self.get_enemy_completed_units_by_type(
+            obs, units.Terran.Barracks)
+        enemy_marines = self.get_enemy_units_by_type(obs, units.Terran.Marine)
+
+        return (len(command_centers),
+                len(scvs),
+                len(idle_scvs),
+                len(supply_depots),
+                len(completed_supply_depots),
+                len(barrackses),
+                len(completed_barrackses),
+                len(marines),
+                queued_marines,
+                free_supply,
+                can_afford_supply_depot,
+                can_afford_barracks,
+                can_afford_marine,
+                len(enemy_command_centers),
+                len(enemy_scvs),
+                len(enemy_idle_scvs),
+                len(enemy_supply_depots),
+                len(enemy_completed_supply_depots),
+                len(enemy_barrackses),
+                len(enemy_completed_barrackses),
+                len(enemy_marines))
+
+    def step(self, obs):
+        super(SmartAgent, self).step(obs)
+        state = str(self.get_state(obs))
+        action = self.qtable.choose_action(state)
+        if self.should_log_actions : fh_actions.write(",action=%s," % action)
+        #if self.should_log_actions: fh_actions.write("\r\nstep,agent=%s,action=%s," % (self.agent_name, action))
+        if self.previous_action is not None:
+            self.qtable.learn(self.previous_state,
+                              self.previous_action,
+                              obs.reward,
+                              'terminal' if obs.last() else state)
+
+        # Record current learnings
+        if obs.last():
+            logger.info('Record current learnings')
+            self.qtable.q_table.to_pickle(DATA_FILE, 'gzip')
+
+        self.previous_state = state
+        self.previous_action = action
+
+        return getattr(self, action)(obs)
+
+
+def main(unused_argv):
+    agent1 = SmartAgent()
+    agent2 = RandomAgent()
+    try:
+        with sc2_env.SC2Env(
+                map_name="Simple64",
+                players=[sc2_env.Agent(sc2_env.Race.terran), sc2_env.Agent(sc2_env.Race.terran)],
+                #players=[sc2_env.Agent(sc2_env.Race.terran), sc2_env.Bot(sc2_env.Race.terran, sc2_env.Difficulty.very_easy)],
+                agent_interface_format=features.AgentInterfaceFormat(
+                    action_space=actions.ActionSpace.RAW,
+                    use_raw_units=True,
+                    raw_resolution=64,
+                ),
+                step_mul=48,
+                disable_fog=True,
+        ) as env:
+            #run_loop.run_loop([agent1], env, max_episodes=10000)
+            run_loop.run_loop([agent1, agent2], env, max_episodes=1000)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(main)
