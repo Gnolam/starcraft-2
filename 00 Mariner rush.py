83fe6b7c07ad6255e19f9f760b42a3f2e104cafe
Alex Skorokhod <oleksiy.skorokhod@gmail.com>
2020-01-24 15:45:21 +1100

    Re-structure. x2 L2 agents are in own files as well

--- 00 Mariner rush.py
+++ 00 Mariner rush.py
@@ -6,14 +6,15 @@ from pysc2.agents import base_agent
 from pysc2.lib import actions, features, units
 from pysc2.env import sc2_env, run_loop
 
-from agents.l1_class import L1Agent
+# from agents.l1_class import L1Agent
+from agents.l2_econ import L2AgentBob
+from agents.l2_war import L2AgentPeps
 
 import logging
 
 run_id = 'v23a_consistent'
 
 ########################################################################################################################
-
 
 DQN_econ = 'DQNs/%s_econ.gz' % run_id
 DQN_war = 'DQNs/%s_war.gz' % run_id
@@ -42,197 +43,27 @@ fh_global_debug = open('logs/%s_global.log' % run_id, "a+")
 consistent_econ = False
 consistent_war = False
 
-
 ########################################################################################################################
-
-
-
-########################################################################################################################
-
-
-
-
-########################################################################################################################
-
-
-class L2AgentBob(L1Agent):
-    action_list = (
-        "econ_do_nothing",
-        "econ_harvest_minerals",
-        "econ_build_supply_depot",
-        "econ_build_barracks",
-        "econ_train_marine"
-    )
-
-    agent_name = "Bob"
-    DQN_filename = DQN_econ
-    fh_decisions = fh_econ_decisions
-    fh_state_csv = fh_econ_state_csv
-    consistent_decision_agent = consistent_econ
-
-    def __init__(self, logger):
-        super(L2AgentBob, self).__init__(logger)
-
-    def step(self, obs):
-        ## print("step at L2AgentBob (%s)" % self.agent_name)
-        return super(L2AgentBob, self).step(obs)
-
-    def get_state(self, obs):
-        # info for counters
-        scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
-        idle_scvs = [scv for scv in scvs if scv.order_length == 0]
-        command_centers = self.get_my_units_by_type(obs, units.Terran.CommandCenter)
-        supply_depots = self.get_my_units_by_type(obs, units.Terran.SupplyDepot)
-        completed_supply_depots = self.get_my_completed_units_by_type(
-            obs, units.Terran.SupplyDepot)
-        barrackses = self.get_my_units_by_type(obs, units.Terran.Barracks)
-        completed_barrackses = self.get_my_completed_units_by_type(
-            obs, units.Terran.Barracks)
-        marines = self.get_my_units_by_type(obs, units.Terran.Marine)
-
-        queued_marines = (completed_barrackses[0].order_length
-                          if len(completed_barrackses) > 0 else 0)
-
-        free_supply = (obs.observation.player.food_cap -
-                       obs.observation.player.food_used)
-        can_afford_supply_depot = obs.observation.player.minerals >= 100
-        can_afford_barracks = obs.observation.player.minerals >= 150
-        can_afford_marine = obs.observation.player.minerals >= 50
-
-        enemy_scvs = self.get_enemy_units_by_type(obs, units.Terran.SCV)
-        enemy_command_centers = self.get_enemy_units_by_type(obs, units.Terran.CommandCenter)
-        enemy_supply_depots = self.get_enemy_units_by_type(obs, units.Terran.SupplyDepot)
-        enemy_barrackses = self.get_enemy_units_by_type(obs, units.Terran.Barracks)
-        enemy_factories = self.get_enemy_units_by_type(obs, units.Terran.Factory)
-        enemy_starport = self.get_enemy_units_by_type(obs, units.Terran.Starport)
-
-        # state_dict = {
-        #     "command_centers": len(command_centers),
-        #     "scvs": len(scvs),
-        #     "idle_scvs": len(idle_scvs),
-        #     "supply_depots": len(supply_depots),
-        #     "completed_supply_depots": len(completed_supply_depots),
-        #     "barrackses": len(barrackses),
-        #     "completed_barrackses": len(completed_barrackses),
-        #     "marines": len(marines),
-        #     "queued_marines": queued_marines,
-        #     "free_supply": free_supply,
-        #     "can_afford_supply_depot": can_afford_supply_depot,
-        #     "can_afford_barracks": can_afford_barracks,
-        #     "can_afford_marine": can_afford_marine,
-        #     "enemy_command_centers": len(enemy_command_centers),
-        #     "enemy_scvs": len(enemy_scvs),
-        #     "enemy_supply_depots": len(enemy_supply_depots),
-        #     "enemy_barrackses": len(enemy_barrackses),
-        #     "enemy_factories ": len(enemy_factories),
-        #     "enemy_starport ": len(enemy_starport),
-        #     "enemy_army_band": enemy_army_band,
-        #     "res":obs.observation.player.minerals
-        # }
-
-        # self.log_actions(",get_state:")
-        # for k, v in state_dict.items():
-        #     self.log_actions(" %s=%s" % (k, v))
-
-        return (len(command_centers),
-                len(scvs),
-                len(idle_scvs),
-                len(supply_depots),
-                len(completed_supply_depots),
-                len(barrackses),
-                len(completed_barrackses),
-                len(marines),
-                queued_marines,
-                free_supply,
-                can_afford_supply_depot,
-                can_afford_barracks,
-                can_afford_marine
-                )
-
-
-
-########################################################################################################################
-
-
-class L2AgentPeps(L1Agent):
-    action_list = ("war_do_nothing", "war_attack")
-    # "war_regroup",
-    # "war_defend",
-
-    agent_name = "Peps"
-    DQN_filename = DQN_war
-    fh_decisions = fh_econ_decisions
-    fh_state_csv = fh_war_decisions
-    consistent_decision_agent = consistent_war
-
-    def __init__(self, logger):
-        super(L2AgentPeps, self).__init__(logger)
-
-    def step(self, obs):
-        return super(L2AgentPeps, self).step(obs)
-
-    def get_state(self, obs):
-        marines = self.get_my_units_by_type(obs, units.Terran.Marine)
-
-        # enemy_scvs = self.get_enemy_units_by_type(obs, units.Terran.SCV)
-        # enemy_command_centers = self.get_enemy_units_by_type(obs, units.Terran.CommandCenter)
-        # enemy_supply_depots = self.get_enemy_units_by_type(obs, units.Terran.SupplyDepot)
-        enemy_barrackses = self.get_enemy_units_by_type(obs, units.Terran.Barracks)
-        enemy_factories = self.get_enemy_units_by_type(obs, units.Terran.Factory)
-        enemy_starport = self.get_enemy_units_by_type(obs, units.Terran.Starport)
-        enemy_marines = self.get_enemy_units_by_type(obs, units.Terran.Marine)
-        enemy_marauders = self.get_enemy_units_by_type(obs, units.Terran.Marauder)
-        enemy_Tanks1 = self.get_enemy_units_by_type(obs, units.Terran.SiegeTank)
-        enemy_Tanks2 = self.get_enemy_units_by_type(obs, units.Terran.SiegeTankSieged)
-        enemy_Hells = self.get_enemy_units_by_type(obs, units.Terran.Hellion)
-
-        enemy_army = \
-            len(enemy_marines) * 1 + \
-            len(enemy_marauders) * 2 + \
-            len(enemy_Tanks1) * 4 + \
-            len(enemy_Tanks2) * 4 + \
-            len(enemy_Hells) * 3
-
-        if enemy_army < 10:
-            enemy_army_band = enemy_army
-        elif enemy_army < 30:
-            enemy_army_band = int((enemy_army - 10) / 3) * 3 + 10
-        else:
-            enemy_army_band = int((enemy_army - 30) / 5) * 5 + 30
-
-        # state_dict = {
-        #     "marines": len(marines),
-        #     "enemy_barrackses": len(enemy_barrackses),
-        #     "enemy_factories ": len(enemy_factories),
-        #     "enemy_starport ": len(enemy_starport),
-        #     "enemy_marines": len(enemy_marines),
-        #     "enemy_marauders": len(enemy_marauders),
-        #     "enemy_Tanks1": len(enemy_Tanks1),
-        #     "enemy_Tanks2": len(enemy_Tanks2),
-        #     "enemy_Hells": len(enemy_Hells),
-        #     "enemy_army": enemy_army,
-        #     "enemy_army_band": enemy_army_band
-        # }
-
-        # self.log_actions(",get_state:")
-        # for k, v in state_dict.items():
-        #     self.log_actions(" %s=%s" % (k, v))
-
-        return (
-            len(marines),
-            enemy_army_band
-        )
-
-
-########################################################################################################################
-
 
 class SmartAgentG2(base_agent.BaseAgent):
     agent_name = "SmartAgent Gen2"
     should_log_actions = True
 
-    AI_Peps = L2AgentPeps(logger)
-    AI_Bob = L2AgentBob(logger)
+    AI_Peps = L2AgentPeps(
+        DQN_filename=DQN_war,
+        fh_decisions=fh_econ_decisions,
+        fh_state_csv=fh_war_decisions,
+        consistent_decision_agent=consistent_war,
+        logger=logger
+    )
+
+    AI_Bob = L2AgentBob(
+        consistent_decision_agent=consistent_econ,
+        DQN_filename=DQN_econ,
+        logger=logger,
+        fh_decisions=fh_econ_decisions,
+        fh_state_csv=fh_econ_state_csv
+    )
 
     def __init__(self):
         super(SmartAgentG2, self).__init__()
