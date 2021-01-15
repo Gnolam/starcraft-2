21093e1a1b52ce3832c51cb12cc32cbffbbebc39
Alex Skorokhod <oleksiy.skorokhod@gmail.com>
2021-01-16 10:33:37 +1100

    start working on gen. Grievous poc

---
+++ l2_grievous.py
@@ -0,0 +1,11 @@
+'''
+ POC stage
+ 2 TFs:
+   - TF1: attack
+   - TF2: reserve
+ 3 states:
+ - wait for TF1 to deplete, attack meanwhile
+ - wait till TF2 >= 10 mariners
+ - (init with: TF2->TF1) 
+'''
+
